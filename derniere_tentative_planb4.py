0af0793ff6181d04192b18f2b682832e4fc41c91
Michael <salahmichael@hotmail.com>
2024-05-10 17:52:05 +0200

    Plan b en bonne roue

--- derniere_tentative_planb4.py
+++ derniere_tentative_planb4.py
@@ -21,53 +21,53 @@ climates = []
 frequencies = []
 hitdices = []
 
-for url in range(1,2):
+for url in range(1,10):
     response = requests.get(list_url[url])
     soup = BeautifulSoup(response.text, "html5lib")
         # peut-être qu'un time sleep de python time serait utile si je mets toutes les pages 
     content = soup.find("table")
 
-    #print(type(content))
+    print(type(content))
     
     x = content.text
     #print(x)
 
-    # les blancs dans la table ne sont pas des div mais des td, inutile d'essayer de les corriger plus bas, il faut le faire sur le content
-   
-    for tr in content.tbody.find_all("tr"):
-        for td in tr.find_all("td"):
-            if td.text.strip() == "":
-                monsters_values.append("None")
-            else:
-                for element in td.find_all("div", class_="valeur"): 
-                    monsters_values.append(element.text.strip())        
     
+
     for element in content.find_all("div", class_="etiquette"):
         monsters_stats.append(element.text.strip())
 
     for element in content.find_all("span", class_="fc_item_title"):
         monsters_names.append(element.text.strip())
 
-    climates = monsters_values[1::23]
-    frequencies = monsters_values[2::23]
-    hitdices = monsters_values[12::23]
-
-    
+    for element in content.find_all("div", class_="valeur"):
+        monsters_values.append(element.text.strip())
 
 #print(response)
 #print(soup)
 #print(soup1)
-#print(content)
+print(content)
 #print(type(content))
 #print(monsters_values)
-print(monsters_values[0])
 print(len(monsters_values))
-print(monsters_names)
+#print(monsters_names)
 print(len(monsters_names))
 #print(monsters_stats)
-#print(len(monsters_stats))
-print(climates)
-print(frequencies)
-print(hitdices)
+print(len(monsters_stats))
 
 
+# ça marche mais il reste deux problèmes:
+    # le compte ne tombe pas juste : il y a clairement des stats en trop ou trop peu (trop peu : 24 en théorie, entre 19 et 20 en moyenne)
+                #les noms et les valeurs se suivent les unes après les autres pour former une seule très longue liste
+    # => je ne sais pas si c'est possible, mais je devrais diviser la liste tous les 25 pas en un nombre indéfini d'autres listes
+
+
+
+
+
+
+
+
+
+#for page in range(1,10):
+ #   result = requests.get(url + str(page)), 
