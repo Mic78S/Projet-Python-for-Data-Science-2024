d37c3f6c2b61a3cb6acee7dedfc19d1151b12757
Michael <salahmichael@hotmail.com>
2024-05-11 00:50:47 +0200

    dataframe plan b ok

--- derniere_tentative_planb3.py
+++ derniere_tentative_planb3.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 root = 20
 
@@ -13,15 +14,36 @@ for i in range(1,10):
     list_url.append(url)
 
 #print(list_url)
+#le site comprend 67 pages... mais ça marche, il faut juste lui laisser un peu de temps
 
 monsters_stats = []
 monsters_values = []
 monsters_names = []
+
 climates = []
 frequencies = []
 hitdices = []
 
-for url in range(1,2):
+organisation = []
+cycle_actif = []
+regime = []
+intelligence = []
+treasure = []
+alignement = []
+nombre = []
+ac = []
+move = []
+taco = []
+nbattacks = []
+degats = []
+special_attacks = []
+special_defenses = []
+magical_resistance = []
+size = []
+moral = []
+xp = []
+
+for url in range(1,10):
     response = requests.get(list_url[url])
     soup = BeautifulSoup(response.text, "html5lib")
         # peut-être qu'un time sleep de python time serait utile si je mets toutes les pages 
@@ -51,23 +73,50 @@ for url in range(1,2):
     climates = monsters_values[1::23]
     frequencies = monsters_values[2::23]
     hitdices = monsters_values[12::23]
-
     
+    df = pd.DataFrame({
+        "Nom": monsters_names,
+        "Climate/Terrain" : climates,
+        "Frequency" : frequencies,
+        "Hit Dices" : hitdices
+    })
+
+    # maintenant j'ai mon dataframe, qu'est-ce que j'en fais ? Compter le nombre d'occurences de chaque valeur par liste et transformer ça en graphique ?
 
 #print(response)
 #print(soup)
 #print(soup1)
 #print(content)
 #print(type(content))
-#print(monsters_values)
-print(monsters_values[0])
-print(len(monsters_values))
-print(monsters_names)
-print(len(monsters_names))
+print(monsters_values)
+#print(monsters_values[0])
+#print(len(monsters_values))
+#print(monsters_names)
+#print(len(monsters_names))
 #print(monsters_stats)
 #print(len(monsters_stats))
-print(climates)
-print(frequencies)
-print(hitdices)
+#print(climates)
+#print(frequencies)
+#print(hitdices)
+#print(df)
 
+
+    #organisation = monsters_values[3::23]
+    #cycle_actif = monsters_values[4::23]
+    #regime = monsters_values[5::23]
+    #intelligence = monsters_values[6::23]
+    #treasure = monsters_values[7::23]
+    #alignement = monsters_values[8::23]
+    #nombre = monsters_values[9::23]
+    #ac = monsters_values[10::23]
+    #move = monsters_values[11::23]
+    #taco = monsters_values[13::23]
+    #nbattacks = monsters_values[14::23]
+    #degats = monsters_values[15::23]
+    #special_attacks = monsters_values[16::23]
+    #special_defenses = monsters_values[17::23]
+    #magical_resistance = monsters_values[18::23]
+    #size = monsters_values[19::23]
+    #moral = monsters_values[20::23]
+    #xp = monsters_values[21::23]
 
